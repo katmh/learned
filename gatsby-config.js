9362284adc201a3bab2e509747d2f2f8ed6b9b01
Katherine Huang <hello@katmh.com>
2019-12-14 02:37:06 -0500

    Connect to Sheety API using gatsby-source-rest-api plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,14 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-rest-api`,
+      options: {
+        endpoints: [
+          'https://v2-api.sheety.co/5fc10cba7d7993c3e6e780a56f916533/learned/sheet'
+        ]
+      }
+    }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const IndexPage = ({data}) => (
+    <div>
+        {data.allRestApi5Fc10Cba7D7993C3E6E780A56F916533LearnedSheet.nodes[0].sheet.map((sheet) => (
+            <p>{sheet.lesson ? sheet.date + ': ' + sheet.lesson : ''}</p>
+        ))}
+    </div>
+)
+
+export const query = graphql`
+query MyQuery {
+    allRestApi5Fc10Cba7D7993C3E6E780A56F916533LearnedSheet {
+      nodes {
+        sheet {
+          date
+          lesson
+        }
+      }
+    }
+  }  
+`
+
+export default IndexPage
